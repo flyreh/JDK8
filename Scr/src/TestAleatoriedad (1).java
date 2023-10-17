6288c333770aa2e4c0d28ae178a6b19031f97337
flyreh <jose.manusc12224@gmail.com>
2023-10-17 12:58:32 -0500

    act

---
+++ Scr/src/TestAleatoriedad (1).java
@@ -0,0 +1,32 @@
+package Extra;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.concurrent.TimeUnit;
+
+public class TestAleatoriedad {
+    public static void main(String[] args) throws InterruptedException {
+        TiempoAleatorio.setRango(5);
+        LocalDateTime aleatorio = TiempoAleatorio.retornarHoraAleatoria();
+        //Date time formatter se usa para colocar el formato de la salida, en este caso
+        //horas:minutos:segundos
+        DateTimeFormatter formato = DateTimeFormatter.ofPattern("HH:mm:ss");
+        //La manera de colocar el formato a una varible de tiempo es con .format()
+        System.out.println(aleatorio.format(formato));
+        //Inicializamos un bucle infinito que se romperá cuando llegue la hora aleatoria generada;
+        while(true){
+            //creamos un objeto llamado ahora que almacenará el tiempo actual
+            LocalDateTime ahora = LocalDateTime.now();
+            System.out.println(ahora.format(formato));
+            //Comprobamos si la hora actual es IGUAL o DESPUES de la hora generada anteriormente
+            //Esto ya que el equals compara también los nanosegundos, por lo que es altamente
+            //probable que las horas no sean iguales
+            if(ahora.equals(aleatorio)||ahora.isAfter(aleatorio)){
+                System.out.println("Hora llegada");
+                break;
+            }
+            //Esta línea hace que el bucle se detenga 1 segundo
+            TimeUnit.SECONDS.sleep(1);
+        }
+    }
+}
---
+++ Scr/src/TestAleatoriedad.java
@@ -0,0 +1,22 @@
+package Extra;
+
+import java.util.ArrayList;
+
+public class TestAleatoriedad {
+    public static void main(String[] args) {
+        Aleatoriedad.setLimite(60);
+        /*for (int i = 0; i < 10; i++) {
+            int numero = Aleatoriedad.retornarNumeroAleatorio();
+            System.out.println("Número aleatorio: " + numero);
+        }*/
+        ArrayList<String> acciones = new ArrayList<>();
+        acciones.add("Dormir");
+        acciones.add("Hablar");
+        acciones.add("Comer");
+        Aleatoriedad.setLimite(acciones.size());
+        for(int i=0; i<20; i++){
+            int ind = Aleatoriedad.retornarNumeroAleatorio();
+            System.out.println("Accion aleatoria: "+acciones.get(ind));
+        }
+    }
+}
